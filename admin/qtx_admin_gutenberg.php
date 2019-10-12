69f5e68bfbf4bcac5a515a03e6162d294c4004cc
HerrVigg <herrvigg@gmail.com>
2019-10-12 13:00:25 +0200

    Create class QTX_Admin_Gutenberg

--- admin/qtx_admin_gutenberg.php
+++ admin/qtx_admin_gutenberg.php
@@ -4,172 +4,192 @@
  * @author: herrvigg
  */
 
-if (!defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-// *//**
-// * Set up a custom REST API controller class for the Page post type.
-// *
-// * @param  array  $args The post type arguments.
-// * @param  string $name The name of the post type.
-// *
-// * @return array $args The post type arguments, possibly modified.
-// */
-//function qtranxf_register_rest_controller( $args, $name ) {
-//	// Tell WordPress to use our KM_REST_Pages_Controller class
-//	// for Page REST API requests.
-//	$args['rest_controller_class'] = 'QTX_REST_Post_Controller';
-//	return $args;
-//}
-//add_filter( 'register_post_type_args', 'qtranxf_register_rest_controller', 10, 2 );
+/**
+ * Class QTX_Admin_Gutenberg
+ *
+ * Manages the Gutenberg block editor with the related REST API
+ */
+class QTX_Admin_Gutenberg {
 
-function qtranxf_rest_prepare($response, $post, $request)
-{
-    global $q_config;
+	/**
+	 * QTX_Admin_Gutenberg constructor
+	 */
+	public function __construct() {
+		//add_filter( 'register_post_type_args', array($this, 'register_rest_controller'), 10, 2 );
 
-    if ($request['context'] !== 'edit' || $request->get_method() !== 'GET') {
-        return $response;
-    }
+		// TODO generalize to selected post types in options
+		$post_type = 'post';
+		add_filter( "rest_prepare_{$post_type}", array( $this, 'rest_prepare' ), 99, 3 );
+		add_filter( 'rest_request_before_callbacks', array( $this, 'rest_request_before_callbacks' ), 99, 3 );
+		add_filter( 'rest_request_after_callbacks', array( $this, 'rest_request_after_callbacks' ), 99, 3 );
 
-    assert(!$q_config['url_info']['doing_front_end']);
+		add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_block_editor_assets)' ) );
+		add_action( 'qtranslate_admin_loadConfig', array( $this, 'load_configuration' ) );
+	}
 
-	// TODO allow user to select editor lang with buttons
-	$editor_lang = $q_config['url_info']['language'];
+//	/**
+//	 * Set up a custom REST API controller class
+//	 *
+//	 * @param array $args The post type arguments.
+//	 * @param string $name The name of the post type.
+//	 *
+//	 * @return array $args The post type arguments, possibly modified.
+//	 */
+//	public function register_rest_controller( $args, $name ) {
+//		$args['rest_controller_class'] = 'QTX_REST_Post_Controller';
+//
+//		return $args;
+//	}
 
-    $response_data = $response->get_data();
-    if (isset($response_data['content']) && is_array($response_data['content']) && isset($response_data['content']['raw'])) {
-        $response_data['title']['raw'] = qtranxf_use($editor_lang, $response_data['title']['raw']);
-        $response_data['content']['raw'] = qtranxf_use($editor_lang, $response_data['content']['raw']);
-        $response_data['qtx_editor_lang'] = $editor_lang;
-        $response->set_data($response_data);
-    }
+	/**
+	 * @param WP_HTTP_Response|WP_REST_Response $response
+	 * @param string $editor_lang
+	 *
+	 * @return mixed
+	 */
+	private function select_raw_response_language($response, $editor_lang) {
+		$response_data = $response->get_data();
+		if ( isset( $response_data['content'] ) && is_array( $response_data['content'] ) && isset( $response_data['content']['raw'] ) ) {
+			$response_data['title']['raw']    = qtranxf_use( $editor_lang, $response_data['title']['raw'] );
+			$response_data['content']['raw']  = qtranxf_use( $editor_lang, $response_data['content']['raw'] );
+			$response_data['qtx_editor_lang'] = $editor_lang;
+			$response->set_data( $response_data );
+		}
 
-    return $response;
-}
+		return $response;
+	}
 
-// TODO generalize to selected post types in options
-$post_type = 'post';
-add_filter("rest_prepare_{$post_type}", 'qtranxf_rest_prepare', 99, 3);
-
-function qtranxf_rest_request_before_callbacks($response, $handler, $request)
-{
-    if ($request->get_method() !== 'PUT' && $request->get_method() !== 'POST') {
-        return $response;
-    }
-
-    $editor_lang = $request->get_param('qtx_editor_lang');
-    if (!isset($editor_lang)) {
-        return $response;
-    }
-
-    $request_body = json_decode($request->get_body(), true);
-    $post = get_post($request->get_param('id'), ARRAY_A);
-
-    $fields = ['content', 'title'];
-    foreach ($fields as $field) {
-
-        if (isset($request_body[$field])) {
-
-            $new_value = $request_body[$field];
-
-            $original_value = $post['post_' . $field];
-            $blocks = qtranxf_get_language_blocks($original_value);
-            if (count($blocks) > 1) {
-	            $split = qtranxf_split_languages( $blocks );
-	            /*
-	            foreach ( $content as $language => $lang_text ) {
-	                $lang_text = trim( $lang_text );
-	                if( ! empty( $lang_text ) ) $result[$language] = $language;
-	            }*/
-	            $split[ $editor_lang ] = $new_value;
-            }
-            else {
-	            global $q_config;
-	            $split = array();
-	            foreach ($q_config['enabled_languages'] as $lang) {
-					$split[$lang] = $original_value;
-	            }
-	            $split[$editor_lang] = $new_value;
-            }
-
-            // TODO handle custom separator
-            //$sep = '[';
-            //$new_data = qtranxf_collect_translations_deep( $split, $sep );
-            //$new_data = qtranxf_join_texts( $split, $sep );
-            $new_data = qtranxf_join_b($split);
-
-            $request->set_param($field, $new_data);
-            //$request_body[ $field ] =  $new_data;
-        }
-    }
-
-    return $response;
-}
-
-add_filter('rest_request_before_callbacks', 'qtranxf_rest_request_before_callbacks', 99, 3);
-
-function qtranxf_rest_request_after_callbacks($response, $handler, $request)
-{
-    if ($request['context'] !== 'edit' || $request->get_method() !== 'PUT' && $request->get_method() !== 'POST') {
-        return $response;
-    }
-
-    $editor_lang = $request->get_param('qtx_editor_lang');
-    if (!isset($editor_lang)) {
-        return $response;
-    }
-
-    $response_data = $response->get_data();
-    if (isset($response_data['content']) && is_array($response_data['content']) && isset($response_data['content']['raw'])) {
-        $response_data['title']['raw'] = qtranxf_use($editor_lang, $response_data['title']['raw']);
-        $response_data['content']['raw'] = qtranxf_use($editor_lang, $response_data['content']['raw']);
-        $response_data['qtx_editor_lang'] = $editor_lang;
-        $response->set_data($response_data);
-    }
-
-    return $response;
-}
-
-add_filter('rest_request_after_callbacks', 'qtranxf_rest_request_after_callbacks', 99, 3);
-
-function qtranxf_enqueue_block_editor_assets()
-{
-    $script_file = 'js/lib/editor-gutenberg';
-    $script_file .= defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '.js' : '.min.js';
-    wp_register_script(
-        'qtx-gutenberg',
-        plugins_url($script_file, __FILE__),
-        array(),
-        filemtime(plugin_dir_path(__FILE__) . $script_file),
-        true
-    );
-    wp_enqueue_script('qtx-gutenberg');
-}
-
-add_action('enqueue_block_editor_assets', 'qtranxf_enqueue_block_editor_assets');
-
-function qtranxf_admin_loadConfigGutenberg() {
-	global $wp_version;
-	require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-	if ( version_compare( $wp_version, '5.0' ) >= 0 &&
-	     ! ( class_exists( 'Classic_Editor' ) ||
-	         is_plugin_active( 'disable-gutenberg/disable-gutenberg.php' ) ||
-	         is_plugin_active( 'no-gutenberg/no-gutenberg.php' ) ) ) {
+	/**
+	 * @param WP_REST_Response $response
+	 * @param WP_Post $post
+	 * @param WP_REST_Request $request
+	 *
+	 * @return mixed
+	 */
+	public function rest_prepare( $response, $post, $request ) {
 		global $q_config;
 
-		if ($q_config['editor_mode'] == QTX_EDITOR_MODE_LSB) {
-			$q_config['editor_mode'] = QTX_EDITOR_MODE_SINGLE;
+		if ( $request['context'] !== 'edit' || $request->get_method() !== 'GET' ) {
+			return $response;
+		}
+
+		assert( ! $q_config['url_info']['doing_front_end'] );
+
+		// TODO allow user to select editor lang with buttons
+		$editor_lang = $q_config['url_info']['language'];
+
+		$response = $this->select_raw_response_language($response, $editor_lang);
+
+		return $response;
+	}
+
+	/**
+	 * @param WP_HTTP_Response $response
+	 * @param array $handler
+	 * @param WP_REST_Request $request
+	 *
+	 * @return mixed
+	 */
+	public function rest_request_before_callbacks( $response, $handler, $request ) {
+		if ( $request->get_method() !== 'PUT' && $request->get_method() !== 'POST' ) {
+			return $response;
+		}
+
+		$editor_lang = $request->get_param( 'qtx_editor_lang' );
+		if ( ! isset( $editor_lang ) ) {
+			return $response;
+		}
+
+		$request_body = json_decode( $request->get_body(), true );
+		$post         = get_post( $request->get_param( 'id' ), ARRAY_A );
+
+		$fields = [ 'content', 'title' ];
+		foreach ( $fields as $field ) {
+
+			if ( isset( $request_body[ $field ] ) ) {
+				$new_value = $request_body[ $field ];
+
+				$original_value = $post[ 'post_' . $field ];
+				$blocks         = qtranxf_get_language_blocks( $original_value );
+				if ( count( $blocks ) > 1 ) {
+					$split = qtranxf_split_languages( $blocks );
+					$split[ $editor_lang ] = $new_value;
+				} else {
+					global $q_config;
+					$split = array();
+					foreach ( $q_config['enabled_languages'] as $lang ) {
+						$split[ $lang ] = $original_value;
+					}
+					$split[ $editor_lang ] = $new_value;
+				}
+
+				// TODO handle custom separator
+				//$sep = '[';
+				//$new_data = qtranxf_collect_translations_deep( $split, $sep );
+				//$new_data = qtranxf_join_texts( $split, $sep );
+				$new_data = qtranxf_join_b( $split );
+
+				$request->set_param( $field, $new_data );
+				//$request_body[ $field ] =  $new_data;
+			}
+		}
+
+		return $response;
+	}
+
+	/**
+	 * @param WP_HTTP_Response $response
+	 * @param array $handler
+	 * @param WP_REST_Request $request
+	 *
+	 * @return mixed
+	 */
+	public function rest_request_after_callbacks( $response, $handler, $request ) {
+		if ( $request['context'] !== 'edit' || $request->get_method() !== 'PUT' && $request->get_method() !== 'POST' ) {
+			return $response;
+		}
+
+		$editor_lang = $request->get_param( 'qtx_editor_lang' );
+		if ( ! isset( $editor_lang ) ) {
+			return $response;
+		}
+
+		$response = $this->select_raw_response_language($response, $editor_lang);
+
+		return $response;
+	}
+
+	public function enqueue_block_editor_assets() {
+		$script_file = 'js/lib/editor-gutenberg';
+		$script_file .= defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '.js' : '.min.js';
+		wp_register_script(
+			'qtx-gutenberg',
+			plugins_url( $script_file, __FILE__ ),
+			array(),
+			filemtime( plugin_dir_path( __FILE__ ) . $script_file ),
+			true
+		);
+		wp_enqueue_script( 'qtx-gutenberg' );
+	}
+
+	public function load_configuration() {
+		global $wp_version;
+		require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+		if ( version_compare( $wp_version, '5.0' ) >= 0 &&
+		     ! ( class_exists( 'Classic_Editor' ) ||
+		         is_plugin_active( 'disable-gutenberg/disable-gutenberg.php' ) ||
+		         is_plugin_active( 'no-gutenberg/no-gutenberg.php' ) ) ) {
+			global $q_config;
+
+			if ( $q_config['editor_mode'] == QTX_EDITOR_MODE_LSB ) {
+				$q_config['editor_mode'] = QTX_EDITOR_MODE_SINGLE;
+			}
 		}
 	}
 }
 
-add_action('qtranslate_admin_loadConfig', 'qtranxf_admin_loadConfigGutenberg');
-
-//function qtranxf_enqueue_editor() {
-//}
-//add_action( 'wp_enqueue_editor', 'qtranxf_enqueue_editor' );
-
-
-//if ( file_exists(QTRANSLATE_DIR.'/dev/slugs' ) )
-//	require_once( QTRANSLATE_DIR.'/dev/slugs/qtx_slug.php' );
+new QTX_Admin_Gutenberg();
